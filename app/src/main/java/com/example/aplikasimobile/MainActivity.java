095b51c952e6988f44714aa57d7cdcc9e6332a2f
GreatD <devamahendrar@gmail.com>
2021-03-22 08:28:07 +0700

    login authentication

--- app/src/main/java/com/example/aplikasimobile/MainActivity.java
+++ app/src/main/java/com/example/aplikasimobile/MainActivity.java
@@ -19,9 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final String[] user = getResources().getStringArray(R.array.emailUser);
+        final String[] pass = getResources().getStringArray(R.array.passUser);
+
         btnSignIn = findViewById(R.id.btnSigIn);
         edEmail = findViewById(R.id.edEmail);
         edPassword = findViewById(R.id.edPassword);
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean found = false;
+                String myUsername = edEmail.getText().toString();
+                String myPassword = edPassword.getText().toString();
+
+                for (int i = 0; i < user.length; i++) {
+                    if (user[i].equals(myUsername) && pass[i].equals(myPassword)) {
+                        Toast.makeText(MainActivity.this,"Login Berhasil",Toast.LENGTH_LONG).show();
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    Toast.makeText(MainActivity.this,"Email atau Password Salah",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
