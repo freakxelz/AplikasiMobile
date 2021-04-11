0f60aded05d757ee0ed6bec1c883c3e45a3f2894
Xuser23 <devamahendrar@gmail.com>
2021-04-11 16:04:04 +0700

    linear layout activity_home.xml

---
+++ app/src/main/java/com/example/aplikasimobile/activity_view_data.java
@@ -0,0 +1,13 @@
+package com.example.aplikasimobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_view_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_data);
+    }
+}
